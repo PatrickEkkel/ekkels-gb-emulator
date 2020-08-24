ce729a2421c753a1787fb54cd15b0d1ca83deeea
Patrick Ekkel <patrick.ekkel@antagonist.nl>
2020-08-24 20:11:22 +0200

    implemented first opcode, fixed the endianness

--- emulator.py
+++ emulator.py
@@ -1,3 +1,4 @@
+import struct
 import opcodes
 from array import array
 
@@ -15,26 +16,104 @@ class MMU:
     def read(self, address):
         return self.bios.data[address]
         # pass in the bios
+    def _getbyte(self,address):
+        return struct.pack('B', self.bios.data[address])
+    def read_u16(self,address):
+        return int.from_bytes(self._getbyte(address) + self._getbyte(address + 1), 'little')
         
+
+class Registers:
+
+    def __init__(self):
+        self.sA(0x00)
+        self.sB(0x00)
+        self.sC(0x00)
+        self.sD(0x00)
+        self.sE(0x00)
+        self.sF(0x00)
+        self.sH(0x00)
+        self.sL(0x00)
+    
+    def sA(self, value):
+        self.a = value
+
+    def sB(self, value):
+        self.b = value
+
+    def sC(self, value):
+        self.c = value
+
+    def sD(self, value):
+        self.d = value
+
+    def sE(self, value):
+        self.e = value
+    
+    def sF(self, value):
+        self.f = value
+    
+    def sH(self, value):
+        self.h = value
+    
+    def sL(self, value):
+        self.l = value
+
+    def gA(self):
+        return self.a
+
+    def gB(self):
+        return self.b
+    
+    def gC(self):
+        return self.c
+
+    def gD(self):
+        return self.d
+
+    def gE(self):
+        return self.e
+    
+    def gF(self):
+        return self.f
+    
+    def gH(self):
+        return self.h
+
+    def gL(self):
+        return self.l
+
 class CPU:
     def __init__(self, mmu):
         self.pc = 0x00
-        self.mmu = mmu
+        self._sp = 0x00
+        self._mmu = mmu
         self.debug_opcode = True
+        self.reg = Registers()
         self.opcodes = [None] * 255
-        self.opcodes[0xFE] = opcodes.CP
-    
+        self.opcodes[0x31] = opcodes.LD16d
+        
     def _read_pc_opcode(self):
-        return self.mmu.read(self.pc)
+        return self._mmu.read(self.pc)
     
     def debug(self, opcode):
-        print("0x{:02x}".format(opcode))
+        return ("0x{:x}".format(opcode))
 
+    def set_sp(self, sp):
+        self._sp = sp
+        
     def step(self):
-        self.pc += 1
+       
         opcode = self._read_pc_opcode()
+        hex =  self.debug(opcode)
         if self.debug_opcode:
-            self.debug(opcode)
-            self.opcodes[opcode](self)
-        #print(str(self.pc))
-
+            print(f'CPU: {hex}')
+        
+        try:
+            self.opcodes[opcode](self._mmu,self)
+        except Exception as e:
+            print(e)
+            print(f'Unknown opcode {hex}')
+               
+                
+        self.pc += 1
+        
--- opcodes.py
+++ opcodes.py
@@ -2,3 +2,13 @@
 
 def CP(cpu):
     pass
+# length: 3 bytes 
+# 0x31 (1 byte) (2 bytes) unsigned
+def LD16d(mmu, cpu):
+    cpu.pc += 1
+    mem = mmu.read_u16(cpu.pc)
+    cpu.set_sp(mem)
+    if cpu.debug_opcode:
+        print(f'CPU: {cpu.debug(mem)}')
+    cpu.pc += 2
+    
